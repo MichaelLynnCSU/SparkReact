7c6df9b53fa3fa55f775c5fcc33329ace7ecee4d
Michael Lynn <hailviral101@yahoo.com>
2019-10-29 14:13:44 -0600

    update
    update

---
+++ T30-1.0/src/main/java/edu/csu2017fa314/T30/Controller/LayoutController.java
@@ -0,0 +1,44 @@
+package edu.csu2017fa314.T30.Controller;
+
+import com.google.gson.Gson;
+import edu.csu2017fa314.T30.Model.Itinerary.Data.DataService;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import static spark.Spark.*;
+
+public class LayoutController {
+
+    VelocityEngine ve;
+    Properties props;
+    DataService myModel;
+    Gson gson;
+
+
+    public LayoutController() {
+
+
+        ve = new VelocityEngine();
+        props = new Properties();
+        props.put("file.resource.loader.path", "C:/Users/aplus/Documents/GitHub/SparkJavascript/T30-1.0/src/main/java/edu/csu2017fa314/T30/View/");
+        ve.init(props);
+
+      //  myModel = new DataService();
+        gson = new Gson();
+
+        get("/layout", (request, response) -> {
+            String htmlFile = "/layout.js";
+           String test =  new String(Files.readAllBytes(Paths.get(getClass().getResource(htmlFile).toURI())), StandardCharsets.UTF_8);
+           return test;
+        });
+
+    }
+
+
+
+
+}
